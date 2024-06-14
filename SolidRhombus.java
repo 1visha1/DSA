cb1f658d63e1c5aa63ee4bde03aa235b82a96394
1visha1 <vishalshekoar999@gmail.com>
2024-06-14 15:06:24 +0530

    Solid Rhombus pattern

--- SolidRhombus.java
+++ SolidRhombus.java
@@ -1,7 +1,20 @@
 public class SolidRhombus{
+    public static int pattern(int n){
 
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<(n-i)-1;j++){
+                System.out.print(" ");
+            }
+            for(int j=0;j<n;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        return 0;
+    }
 
     public static void main(String... args){
-          
+          pattern(5);
     }
 }
